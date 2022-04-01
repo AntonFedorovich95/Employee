aad711e1f698b38cc9220b635541c81e2bac2deb
Anton <anton040795@gmail.com>
2022-04-01 17:14:22 +0400

    2

---
+++ src/main/java/pro/sky/Employee/EmployeeService.java
@@ -0,0 +1,7 @@
+package pro.sky.Employee;
+
+public interface EmployeeService {
+    Employees addEmployee(String firstName, String lastName);
+    Employees removeEmployee(String firstName, String lastName);
+    Employees findEmployee(String firstName, String lastName);
+}
