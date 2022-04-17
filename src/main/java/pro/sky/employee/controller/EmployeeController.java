da01f8c294666327155f6dc698b3de98b78875b1
Anton <anton040795@gmail.com>
2022-04-17 16:41:08 +0400

    5

--- src/main/java/pro/sky/employee/controller/EmployeeController.java
+++ src/main/java/pro/sky/employee/controller/EmployeeController.java
@@ -1,9 +1,6 @@
 package pro.sky.employee.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.sky.employee.service.EmployeeService;
 import pro.sky.employee.data.Employees;
 
@@ -19,22 +16,50 @@ public class EmployeeController {
     }
 
     @GetMapping("/add")
-    public Employees addEmployee(@RequestParam String firstName, @RequestParam String lastName) {
-        return employeeService.addEmployee(firstName, lastName);
+    public Employees addEmployeeToList(@RequestParam("lastName") String lastName,
+                                      @RequestParam("firstName") String firstName,
+                                      @RequestParam("department") Integer idDepartment,
+                                      @RequestParam("salary") Integer salaryPerMonth) {
+        return employeeService.addEmployee(lastName, firstName, idDepartment, salaryPerMonth);
     }
 
     @GetMapping("/remove")
-    public Employees removeEmployee(@RequestParam String firstName, @RequestParam String lastName) {
-        return employeeService.removeEmployee(firstName, lastName);
+    public Employees removeEmployeeFromList(@RequestParam String lastName, @RequestParam String firstName) {
+        return employeeService.removeEmployee(lastName, firstName);
     }
 
-    @GetMapping("/find")
-    public Employees findEmployee(@RequestParam String firstName, @RequestParam String lastName) {
-        return employeeService.findEmployee(firstName, lastName);
+    @GetMapping("/find-employee")
+    public Employees findEmployeeInList(@RequestParam String lastName, @RequestParam String firstName) {
+        return employeeService.findEmployee(lastName, firstName);
     }
 
     @GetMapping("/employeelist")
     public List<Employees> printEmployeeList() {
         return employeeService.getEmployeeList();
     }
+
+    @GetMapping("/sum-salary-month")
+    public String sumSalary() {
+        return employeeService.sumSalary();
+    }
+
+    @GetMapping("/departments/max-salary")
+    public List<Employees> employeesMaxSalaryInDepartment(@RequestParam Integer idDepartment) {
+        return employeeService.maxSalaryInDepartment(idDepartment);
+    }
+
+    @GetMapping("/departments/min-salary")
+    public List<Employees> employeesMinSalaryInDepartment(@RequestParam Integer idDepartment) {
+        return employeeService.minSalaryInDepartment(idDepartment);
+    }
+
+    @GetMapping(value = {"/departments/all",  "/departments/all/{idDepartment}"})
+    public List<Employees> createListOfEmployees(@PathVariable(required = false) Integer idDepartment) {
+        if (idDepartment != null) {
+            return employeeService.findEmployeesIdDepartment(idDepartment);
+        } else {
+            return employeeService.sortListEmployeesDepartment();
+        }
+    }
+
 }
--- src/main/java/pro/sky/employee/service/EmployeeService.java
+++ src/main/java/pro/sky/employee/service/EmployeeService.java
@@ -5,8 +5,20 @@ import pro.sky.employee.data.Employees;
 import java.util.List;
 
 public interface EmployeeService {
-    Employees addEmployee(String firstName, String lastName);
-    Employees removeEmployee(String firstName, String lastName);
-    Employees findEmployee(String firstName, String lastName);
+    Employees addEmployee(String lastName, String firstName, Integer idDepartment, Integer salaryPerMonth);
+
+    Employees removeEmployee(String lastName, String firstName);
+
+    Employees findEmployee(String lastName, String firstName);
+
     List<Employees> getEmployeeList();
+
+    String sumSalary();
+    List<Employees> maxSalaryInDepartment(Integer idDepartment);
+
+    List<Employees> findEmployeesIdDepartment(Integer idDepartment);
+
+    List<Employees> minSalaryInDepartment(Integer idDepartment);
+
+    List<Employees> sortListEmployeesDepartment();
 }
